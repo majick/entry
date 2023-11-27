7a4bdada8c3127805ab545ec633409ac3002e4aa
hkau <no_reply_in@sentrytwo.com>
2023-11-27 00:13:09 -0500

    [fix] executable: don't self-destruct

--- src/Executable.ts
+++ src/Executable.ts
@@ -23,8 +23,13 @@ process.env.IMPORT_DIR! =
 if (!fs.existsSync(process.env.IMPORT_DIR))
     fs.mkdirSync(process.env.IMPORT_DIR, { recursive: true });
 
+// check for "_entry_old" file
+const _entry_old_path = path.resolve(path.dirname(process.execPath), "_entry_old");
+if (fs.existsSync(_entry_old_path)) fs.rmSync(_entry_old_path);
+
 // update executable
 const ExpectedExecutable = `entry-${process.platform}-${process.arch}`;
+let NeedsExecutableUpdate = false;
 
 try {
     const Executables = await (
@@ -47,80 +52,40 @@ try {
         const Name = path.basename(process.execPath); // name to save the executable as
         const Dir = path.dirname(process.execPath); // path to save the executable in
 
-        // ...self destruct
-        fs.rmSync(process.execPath);
+        // ...mark current executable as old
+        fs.renameSync(process.execPath, path.join(Dir, "_entry_old"));
 
         // ...download new
-        Bun.spawnSync({
-            cmd: `curl https://sentrytwo.com/api/dist/${ExpectedExecutable} -o "${Dir}/${Name}"`.split(
+        console.log("\x1b[30;100m info \x1b[0m Installing updated executable...");
+        console.log(
+            "\x1b[30;100m info \x1b[0m Executable source:",
+            `https://sentrytwo.com/api/dist/${ExpectedExecutable}`
+        );
+
+        NeedsExecutableUpdate = true;
+        Bun.spawn({
+            cmd: `curl https://sentrytwo.com/api/dist/${ExpectedExecutable} -o ${Name}`.split(
                 " "
             ),
+            cwd: Dir,
             env: process.env,
-            onExit: () => process.exit(),
+            onExit: () => {
+                // make file executable
+                Bun.spawn({
+                    cmd: `chmod +x ./${Name}`.split(" "),
+                    cwd: Dir,
+                    env: process.env,
+                    onExit: () => {
+                        // exit
+                        console.log(
+                            "\x1b[30;42m info \x1b[0m Entry executable updated!"
+                        );
+
+                        process.exit();
+                    },
+                });
+            },
         });
-
-        console.log("\x1b[30;42m info \x1b[0m Entry executable updated!");
-        process.exit();
-    }
-} catch {
-    throw new Error(
-        `\x1b[30;41m error \x1b[0m Failed to start in executable mode!\nUnable to check executable hash!`
-    );
-}
-
-// update assets
-try {
-    if (path.basename(process.execPath).startsWith("entry")) {
-        // download files
-        const RequiredFiles = await (
-            await fetch("https://sentrytwo.com/api/hashes")
-        ).json();
-
-        // ...check current files (remove unneeded)
-        for (const file of fs.readdirSync(process.env.IMPORT_DIR!))
-            if (!RequiredFiles[file]) {
-                // ...delete file
-                fs.rmSync(path.resolve(process.env.IMPORT_DIR!, file));
-
-                // ...log
-                console.log(
-                    `\x1b[30;100m sync \x1b[0m Removing unneeded asset: "${file}"\x1b[0m`
-                );
-            }
-
-        // ...check files
-        for (const file of Object.entries(RequiredFiles)) {
-            // 0: file name, 1: hash
-
-            // check if file exists locally
-            const FilePath = path.resolve(process.env.IMPORT_DIR!, file[0]);
-
-            if (fs.existsSync(FilePath)) {
-                // check hash
-                const CurrentHash = CreateHash(await Bun.file(FilePath).text());
-                if (CurrentHash === file[1]) continue;
-
-                // ...delete file
-                fs.rmSync(FilePath);
-
-                // ...allow execution of the "download file" part
-                console.log(
-                    `\x1b[30;100m sync \x1b[0m Syncing asset: "${file[0]}"\x1b[0m`
-                );
-            }
-
-            // download file
-            await Bun.write(
-                FilePath,
-                await fetch(`https://sentrytwo.com/${file[0]}`)
-            );
-
-            console.log(
-                `\x1b[30;100m sync \x1b[0m Asset synced: "${file[0]}"\x1b[0m`
-            );
-
-            continue;
-        }
     }
 } catch (err) {
     throw new Error(
@@ -128,12 +93,77 @@ try {
     );
 }
 
-// start server from "index.js" (should have been downloaded previously)
-const IndexPath = path.resolve(process.env.IMPORT_DIR, "index.js");
+// ...
+if (!NeedsExecutableUpdate) {
+    // update assets
+    try {
+        if (path.basename(process.execPath).startsWith("entry")) {
+            // download files
+            const RequiredFiles = await (
+                await fetch("https://sentrytwo.com/api/hashes")
+            ).json();
 
-if (!fs.existsSync(IndexPath))
-    throw new Error(
-        `\x1b[30;41m error \x1b[0m Failed to start in executable mode!\nAsset "index.js" does not exist! Please make sure it was synced properly.`
-    );
+            // ...check current files (remove unneeded)
+            for (const file of fs.readdirSync(process.env.IMPORT_DIR!))
+                if (!RequiredFiles[file]) {
+                    if (file.startsWith("entry")) continue;
 
-await import(IndexPath);
+                    // ...delete file
+                    fs.rmSync(path.resolve(process.env.IMPORT_DIR!, file));
+
+                    // ...log
+                    console.log(
+                        `\x1b[30;100m sync \x1b[0m Removing unneeded asset: "${file}"\x1b[0m`
+                    );
+                }
+
+            // ...check files
+            for (const file of Object.entries(RequiredFiles)) {
+                // 0: file name, 1: hash
+
+                // check if file exists locally
+                const FilePath = path.resolve(process.env.IMPORT_DIR!, file[0]);
+
+                if (fs.existsSync(FilePath)) {
+                    // check hash
+                    const CurrentHash = CreateHash(await Bun.file(FilePath).text());
+                    if (CurrentHash === file[1]) continue;
+
+                    // ...delete file
+                    fs.rmSync(FilePath);
+
+                    // ...allow execution of the "download file" part
+                    console.log(
+                        `\x1b[30;100m sync \x1b[0m Syncing asset: "${file[0]}"\x1b[0m`
+                    );
+                }
+
+                // download file
+                await Bun.write(
+                    FilePath,
+                    await fetch(`https://sentrytwo.com/${file[0]}`)
+                );
+
+                console.log(
+                    `\x1b[30;100m sync \x1b[0m Asset synced: "${file[0]}"\x1b[0m`
+                );
+
+                continue;
+            }
+        }
+    } catch (err) {
+        throw new Error(
+            `\x1b[30;41m error \x1b[0m Failed to start in executable mode!\n${err}`
+        );
+    }
+
+    // start server from "index.js" (should have been downloaded previously)
+    const IndexPath = path.resolve(process.env.IMPORT_DIR, "index.js");
+
+    if (!fs.existsSync(IndexPath))
+        throw new Error(
+            `\x1b[30;41m error \x1b[0m Failed to start in executable mode!\nAsset "index.js" does not exist! Please make sure it was synced properly.`
+        );
+
+    await import(IndexPath);
+}
